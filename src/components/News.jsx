4a8ecd711226521324853e31748636544cbc2799
Dan Deng <hui_danny_deng@hotmail.com>
2021-11-29 15:35:09 -0500

    add dropdown filter to news page

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import { Typography, Row, Col, Card, Avatar } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Row, Col, Card, Avatar, Select } from 'antd';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Loader } from './Loader';
+
 import moment from 'moment';
 
 const demoImage =
   'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 const { Title, Text } = Typography;
+const { Option } = Select;
 
 export const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
   const { data: cryptoNews, isLoading } = useGetCryptoNewsQuery({
-    newsCategory: 'Cryptocurrency',
+    newsCategory,
     count: simplified ? 6 : 24,
   });
+  const { data } = useGetCryptosQuery(100);
+
+  if (!cryptoNews?.value) return <Loader />;
   console.log(cryptoNews);
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) > 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews?.value.map((newsItem, i) => (
-        <Col xs={24} lg={8} key={i}>
+        <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
             <a href={newsItem.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
@@ -35,23 +62,22 @@ export const News = ({ simplified }) => {
                   : newsItem.description}
               </p>
               <div className="provider-container">
-	       <div>
-                <Avatar
-                  src={
-                    newsItem?.provider[0]?.image?.thumbnail?.contentUrl ||
-                    demoImage
-                  }
-                  alt="news"
-                />
-                <Text className="provider-name">
-                  {newsItem.provider[0]?.name}
+                <div>
+                  <Avatar
+                    src={
+                      newsItem?.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                    alt="news"
+                  />
+                  <Text className="provider-name">
+                    {newsItem.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(newsItem.datePublished).startOf('ss').fromNow()}
                 </Text>
-		</div>
-		<Text>
-                {moment(newsItem.datePublished).startOf('ss').fromNow()}
-              </Text>
               </div>
-              
             </a>
           </Card>
         </Col>
