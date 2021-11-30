eac1afe13ba97213cb8b67e4b11bcb3b165121b7
danhdeng <hui_danny_deng@hotmail.com>
2021-11-29 23:10:25 -0500

    add LineChart component to show price chart with react-chartjs-2

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -20,7 +20,7 @@ import {
   useGetCryptoHistoryQuery,
 } from '../services/cryptoApi';
 import { Loader } from './Loader';
-import LineChart from './LineChart';
+import { LineChart } from './LineChart';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -114,6 +114,11 @@ export const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
+      <LineChart
+        coinHistory={coinHistory}
+        currency={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,5 +1,78 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Col, Row, Typography } from 'antd';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-export const LineChart = () => {
-  return <div></div>;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+    coinTimestamp.push(
+      new Date(coinHistory.data.history[i].timestamp).toLocaleDateString()
+    );
+    coinPrice.push(coinHistory.data.history[i].price);
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price in USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+  return (
+    <>
+      <Row className="chart-header">
+        <Typography.Title level={2} className="chart-Typography.Title">
+          {coinName} Price Chart
+        </Typography.Title>
+        <Col className="chart-container">
+          <Typography.Title level={5} className="chart-Typography.Title">
+            {coinHistory?.data?.change}
+          </Typography.Title>
+          <Typography.Title level={5} className="chart-Typography.Title">
+            Current {coinName} Price: $ {currentPrice}
+          </Typography.Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
 };
